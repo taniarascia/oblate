a1a1ac458bf7bbaa38d517f0edee9f7602f53283
Tania <taniarascia@gmail.com>
2015-09-29 22:31:48 -0500

    Tracking

--- wp-content/themes/oblate/header.php
+++ wp-content/themes/oblate/header.php
@@ -15,6 +15,19 @@
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700,900' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,500,700' rel='stylesheet' type='text/css'>
 	<?php wp_head(); ?>
+	
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42068444-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+	
+	
 </head>
 
 <body>
