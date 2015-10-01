94b44475fb9f732ea5f0c0bcfc54ae9370fec271
Tania <taniarascia@gmail.com>
2015-09-30 21:13:43 -0500

    Code styling

--- wp-content/themes/oblate/single.php
+++ wp-content/themes/oblate/single.php
@@ -3,23 +3,23 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>">
 				<div class="center">
-				<h2><?php the_title();?></h2>
-				<time>
-					<?php the_time('F j, Y');?>
-				</time>
-				<?php the_tags('&nbsp;☍&nbsp;<span class="tags">', ', ', '', '</span>'); ?>
+					<h2><?php the_title();?></h2>
+					<time>
+						<?php the_time('F j, Y');?>
+					</time>
+					<?php the_tags('&nbsp;☍&nbsp;<span class="tags">', ', ', '', '</span>'); ?>
 				</div>
-					<p>
-						<?php the_content(); ?>
-					</p>
-					<hr>
-					<?php 
+				<p>
+					<?php the_content(); ?>
+				</p>
+			</article>
+			<?php 
 			// If comments are open or we have at least one comment, load up the comment template. 
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;?>
-			</article>
-			<?php	// End the loop.
+
+				<?php	// End the loop.
 			endwhile;
 		endif;
 		?>
