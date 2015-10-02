8c45a291f16f2633d4f12fc195f88c23a062472b
Tania <taniarascia@gmail.com>
2015-10-02 13:59:35 -0500

    Escape HTML

--- wp-content/themes/oblate/functions.php
+++ wp-content/themes/oblate/functions.php
@@ -25,3 +25,52 @@ function new_excerpt_more( $more ) {
 	return '...';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
+
+// Escape HTML
+function escapeHTML($arr) {
+	
+	// last params (double_encode) was added in 5.2.3
+	if (version_compare(PHP_VERSION, '5.2.3') >= 0) {
+	
+		$output = htmlspecialchars($arr[2], ENT_NOQUOTES, get_bloginfo('charset'), false); 
+	}
+	else {
+		$specialChars = array(
+            '&' => '&amp;',
+            '<' => '&lt;',
+            '>' => '&gt;'
+		);
+		
+		// decode already converted data
+		$data = htmlspecialchars_decode($arr[2]);
+
+		// escapse all data inside <pre>
+		$output = strtr($data, $specialChars);
+	}
+
+	if (! empty($output)) {
+		return  $arr[1] . $output . $arr[3];
+	}
+	else
+	{
+		return  $arr[1] . $arr[2] . $arr[3];
+	}
+	
+}
+
+
+function filterCode($data) {
+
+	$modifiedData = preg_replace_callback('@(<pre.*>)(.*)(<\/pre>)@isU', 'escapeHTML', $data);
+	$modifiedData = preg_replace_callback('@(<code.*>)(.*)(<\/code>)@isU', 'escapeHTML', $modifiedData);
+	$modifiedData = preg_replace_callback('@(<tt.*>)(.*)(<\/tt>)@isU', 'escapeHTML', $modifiedData);
+ 
+	return $modifiedData;
+}
+
+
+add_filter( 'content_save_pre', 'filterCode', 9 );
+add_filter( 'excerpt_save_pre', 'filterCode', 9 );
+
+
+?>
