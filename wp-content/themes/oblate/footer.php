7b5552bb7d791b01d4c268ea09a1b2e082f94bf7
Tania <taniarascia@gmail.com>
2015-10-03 11:23:02 -0500

    Highlighting

--- wp-content/themes/oblate/footer.php
+++ wp-content/themes/oblate/footer.php
@@ -1,7 +1,7 @@
 <footer>
 </footer>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.6/highlight.min.js"></script>
+<script src="<?php bloginfo('template_directory');?>/js/prism.js"></script>
 <script src="<?php bloginfo('template_directory');?>/js/script.min.js"></script>
 <?php wp_footer(); ?>
 	</body>
