4a08f6984790635b2fab408a7867f3bc6b5adab0
Tania <taniarascia@gmail.com>
2015-10-25 21:29:58 -0500

    Hover

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -1,12 +1,11 @@
-<article id="post-<?php the_ID(); ?>" class="article-selected">
+<a href="<?php the_permalink();?>" class="article-selected">
+	<article id="post-<?php the_ID(); ?>">
 	<div class="article-header center">
-		<h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-		<time>
-			<?php the_time('F j, Y');?>
-		</time>
-		<?php the_tags('&nbsp;☍&nbsp;<span class="tags">', ', ', '', '</span>'); ?>
-	</div>
-	<a href="<?php the_permalink();?>" class="article-selected">
-		<?php the_excerpt(); ?>
+		<h2><?php the_title();?></h2>
+<time>
+	<?php the_time('F j, Y');?>
+</time>
+</div>
+<?php the_excerpt(); ?>
+	</article>
 	</a>
-</article>
