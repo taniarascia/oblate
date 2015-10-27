cce5ff8973cc41b84b3de95108f990af0f233c8b
Tania <taniarascia@gmail.com>
2015-10-26 21:03:50 -0500

    My own button

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -7,4 +7,5 @@
 		<?php the_tags('&nbsp;☍&nbsp;<span class="tags">', ', ', '', '</span>'); ?>
 	</div>
 	<?php the_content(); ?>
+		<a href="http://twitter.com/intent/tweet?text=<?php echo the_title()?>&url=<?php the_permalink();?>&via=taniarascia" target="_blank" title="Share to Twitter"><img src="<?php bloginfo('template_directory');?>/images/tweetbutton.png" class="twitter-button"></a>
 </article>
