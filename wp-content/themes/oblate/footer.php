a4d90b7b6c465b5d5196d36a9735af401beacf93
Tania <taniarascia@gmail.com>
2015-11-02 14:55:31 -0600

    Facebook

--- wp-content/themes/oblate/footer.php
+++ wp-content/themes/oblate/footer.php
@@ -3,6 +3,14 @@
 <script src="<?php bloginfo('template_directory');?>/js/prism.js"></script>
 <script src="<?php bloginfo('template_directory');?>/js/script.min.js"></script>
 <?php wp_footer(); ?>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 	</body>
 
 	</html>
