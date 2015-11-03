c512789059e79a72ea467f2413abe8ed02b9e3d9
Tania <taniarascia@gmail.com>
2015-11-02 22:42:58 -0600

    I have no idea

--- grunt/src/js/script.js
+++ grunt/src/js/script.js
@@ -5,6 +5,11 @@
 
 })(jQuery);
 
+setCookie ('stylesheet', 1, 60 * 60 * 24 * 365); // Sets cookie with a value of 1 valid for 1 year
+
+var style = getCookie('stylesheet'); // Gets cookie value
+document.getElementById("new-moon").href = "http://www.taniarascia.com/wp-content/themes/oblate/css/newmoon.css";
+ 
 function toggleStyle() {
 	var el = document.getElementById("new-moon");
 	if (el.href.match("http://www.taniarascia.com/wp-content/themes/oblate/css/none.css")) {
