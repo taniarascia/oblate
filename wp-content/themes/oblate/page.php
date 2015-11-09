431c6a62a5d78de597d509bdb3296df3210ac4b3
Tania <taniarascia@gmail.com>
2015-11-09 00:26:38 -0600

    Fix

--- wp-content/themes/oblate/page.php
+++ wp-content/themes/oblate/page.php
@@ -1,16 +1,25 @@
 <?php get_header();?>
-	<main>
+	<main class="large-container">
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();?>
+
 			<article id="post-<?php the_ID(); ?>">
 				<h2><?php the_title();?></h2>
-				<p>
-					<?php the_content(); ?>
-				</p>
+				<div class="row">
+					<div class="column _6 pad">
+						<p>
+							<?php the_content(); ?>
+						</p>
+					</div>
+					<div class="column _3 pad center">
+						<img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>">
+					</div>
+				</div>
 			</article>
+
 			<?php // End the loop.
 		endwhile;
 		?>
 	</main>
-	<?php get_footer();?>
+	<?php get_footer();?> 
