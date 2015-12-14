5345ab8d4ffe76fb4b6e31e715b2b419c541b217
Tania <taniarascia@gmail.com>
2015-12-14 00:27:57 -0600

    Thumbnails

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -1,11 +1,12 @@
 <article id="post-<?php the_ID(); ?>" class="single-article">
 	<div class="article-header center">
+				<?php  $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
+	 $url = $thumb['0']; if (get_post_thumbnail_id()) {?><img src="<?php echo $url;?>" class="article-image"><?php } ?>
 		<h2><?php the_title();?></h2>
 		<time>
 			<?php the_time('F j, Y');?>
 		</time>
 		<?php the_tags('&nbsp;☍&nbsp;<span class="tags">', ', ', '', '</span>'); ?>
-			<?php if (get_post_thumbnail_id()) {?><img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) );?>" style="margin-top:30px;"><?php } ?>
 	</div>
 	<?php the_content(); ?>
 		<div class="alert">
