eed6eeb7be7b8d45cd981080049b5657792b09e3
Tania <taniarascia@gmail.com>
2015-12-14 17:19:32 -0600

    Maybe

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -1,8 +1,10 @@
+<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
+	 $url = $thumb['0']; ?>
 <a href="<?php the_permalink();?>">
 <article id="post-<?php the_ID(); ?>" class="article-excerpt">
 <?php if(get_post_thumbnail_id()) {?>
-  <div class="flex">
-    <div class="box three-fourths"> <?php } ?>		
+  <div class="flex-responsive">
+    <div class="box three-fourths"><img src="<?php echo $url; ?>" class="mobile-only"> <?php } ?>		
 	<div class="article-header">
 		<h2><?php the_title();?></h2>
     <time datetime="<?php the_time('Y-m-d');?>">
@@ -11,11 +13,9 @@
 	</div>
 <?php the_excerpt(); ?>
 <?php if(get_post_thumbnail_id()) {?>
-	<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
-	 $url = $thumb['0']; ?>
 	 </div>
     <div class="box one-fourth thumb">
-	 <img src="<?php echo $url; ?>" class="rescale" alt="<?php the_title();?>">
+	 <img src="<?php echo $url; ?>" class="desktop-only" alt="<?php the_title();?>">
 	  </div>
 	</div><?php } ?>
 </article>
