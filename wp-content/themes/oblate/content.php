e50b72d901268b09ca54468a4dd8de930bafcbd9
Tania <taniarascia@gmail.com>
2015-12-14 12:01:38 -0600

    Flex

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -1,6 +1,8 @@
 <a href="<?php the_permalink();?>">
 <article id="post-<?php the_ID(); ?>" class="article-excerpt">
-<?php if(get_post_thumbnail_id()) {?><div class="flex-responsive"><div class="box three-fourths"> <?php } ?>		
+<?php if(get_post_thumbnail_id()) {?>
+  <div class="flex">
+    <div class="box three-fourths"> <?php } ?>		
 	<div class="article-header">
 		<h2><?php the_title();?></h2>
 		<time><?php the_time('F j, Y');?></time>
@@ -8,8 +10,11 @@
 <?php the_excerpt(); ?>
 <?php if(get_post_thumbnail_id()) {?>
 	<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail' );
-	 $url = $thumb['0']; ?></div><div class="box one-fourth thumb">
+	 $url = $thumb['0']; ?>
+	 </div>
+    <div class="box one-fourth thumb">
 	 <img src="<?php echo $url; ?>" class="rescale">
-	</div></div><?php } ?>
+	  </div>
+	</div><?php } ?>
 </article>
 </a>
