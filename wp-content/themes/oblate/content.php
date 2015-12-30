876e2341ec0e73dcd487bea1e4265c4055758c7e
Tania <taniarascia@gmail.com>
2015-12-30 09:30:41 -0600

    Image replacement

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -2,15 +2,15 @@
 	 $url = $thumb['0']; ?>
 <a href="<?php the_permalink();?>">
 <article id="post-<?php the_ID(); ?>" class="article-excerpt">
-<?php if(get_post_thumbnail_id()) {?>
-  <div class="flex-responsive">
-    <div class="box three-fourths"><img src="<?php echo $url; ?>" class="mobile-only"> <?php } ?>		
 	<div class="article-header">
 		<h2><?php the_title();?></h2>
     <time datetime="<?php the_time('Y-m-d');?>">
 			<?php the_time('F j, Y');?>
 		</time>
 	</div>
+<?php if(get_post_thumbnail_id()) {?>
+  <div class="flex-responsive">
+    <div class="box three-fourths"><img src="<?php echo $url; ?>" class="mobile-only"> <?php } ?>		
 <?php the_excerpt(); ?>
 <?php if(get_post_thumbnail_id()) {?>
 	 </div>
