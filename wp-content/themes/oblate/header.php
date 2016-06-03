89a6424faf83435bb48a67e6d8a27efec5432131
Tania <taniarascia@gmail.com>
2016-06-03 17:37:46 -0500

    Search page

--- wp-content/themes/oblate/header.php
+++ wp-content/themes/oblate/header.php
@@ -6,24 +6,24 @@
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 
-	<script>
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-				(i[r].q = i[r].q || []).push(arguments)
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+		<script>
+			(function (i, s, o, g, r, a, m) {
+				i['GoogleAnalyticsObject'] = r;
+				i[r] = i[r] || function () {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+				a = s.createElement(o),
+					m = s.getElementsByTagName(o)[0];
+				a.async = 1;
+				a.src = g;
+				m.parentNode.insertBefore(a, m)
+			})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-		ga('create', 'UA-42068444-1', 'auto');
-		ga('send', 'pageview');
-	</script>
+			ga('create', 'UA-42068444-1', 'auto');
+			ga('send', 'pageview');
+		</script>
 
 </head>
 
@@ -42,3 +42,11 @@
 			<a href="<?php echo site_url(); ?>/projects" class="six">Projects</a>
 		</div>
 	</nav>
+
+	<form role="search" method="get" class="search-form" action="<?php echo site_url(); ?>">
+		<label>
+			<i class="fa fa-search" aria-hidden="true"></i>
+			<input type="search" class="search-field" placeholder="Search" value="" name="s" >
+		</label>
+		<input type="submit" class="search-submit" value="Search" />
+	</form>
---
+++ wp-content/themes/oblate/search.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+	<main class="main-content">
+
+		<section class="search-page">
+		
+			<div class="small-container">
+
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+
+						get_template_part( 'content', get_post_format() );
+
+						endwhile; else: ?><h3 class="text-center">Sorry, no results were found!</p><?php endif; ?>
+
+			</div>
+			
+		</section>
+
+	</main>
+
+<?php get_footer(); ?>
