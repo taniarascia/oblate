6a7bf01768f2da19b6dc37b30139cc1c673c7d00
Tania <taniarascia@gmail.com>
2016-06-06 10:35:48 -0500

    Search bar

--- wp-content/themes/oblate/comments.php
+++ wp-content/themes/oblate/comments.php
@@ -36,15 +36,13 @@ if ( post_password_required() ) {
 		
 		<?php endif; 
 
-$args = array('comment_notes_after' => '<p style="font-size: .9rem;margin-bottom:1rem;"><a href="https://en.support.wordpress.com/markdown-quick-reference/">Markdown</a> is enabled in comments. If you would like to post code in your comments, please indent your code by four spaces or wrap in three back ticks. HTML/PHP code must be <a href="http://www.freeformatter.com/html-escape.html">escaped</a>. Failure to do so will make me sad. </p>
+$args = array('comment_notes_after' => '<p style="font-size: .9rem;margin-bottom:1rem;"><a href="https://en.support.wordpress.com/markdown-quick-reference/">Markdown</a> is enabled in comments. If you would like to post code in your comments, please indent your code by four spaces. HTML/PHP code must be <a href="http://www.freeformatter.com/html-escape.html">escaped</a>. Failure to do so will make me sad. </p>
 <p style="font-size:.9rem;"><strong>Example:</p></strong>
-<pre>```
-class Voila {
-public:
-  // Voila
-  static const string VOILA = "Voila";
-}
-```
+<pre>    class Voila {
+    public:
+      // Voila
+      static const string VOILA = "Voila";
+    }
 </pre>'); comment_form( $args ); ?>
 
 	</div>
