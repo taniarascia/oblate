2c541abe790436beaa7e09f5660389ea94a2e79f
Tania <taniarascia@gmail.com>
2016-06-21 11:22:55 -0500

    Updates

--- wp-content/themes/oblate/page-home.php
+++ wp-content/themes/oblate/page-home.php
@@ -2,41 +2,43 @@
 
   <main class="home-content">
 
-    <section class="banner">
-
+    <section class="banner text-center">
     </section>
 
     <section class="tutorials">
-
-      <div class="small-container">
-
-        <h2 class="text-center">Tutorials</h2>
-        <p>As I like to call them, "The Lost Guides". </p>
-
+      <div class="content small-container">
+        <h2>Tutorials</h2>
+        <p>The missing instruction manuals for popular web services.</p>
       </div>
 
       <div class="tutorials-grid">
-        <div id="wordpress-tutorial" class="tutorials-cell">
+        <a href="<?php echo site_url(); ?>/developing-a-wordpress-theme-from-scratch/" id="wordpress-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/wptut.png" style="responsive-image">
-        </div>
-        <div id="bootstrap-tutorial" class="tutorials-cell">
+          <h3>WordPress</h3>
+        </a>
+        <a href="<?php echo site_url(); ?>/what-is-bootstrap-and-how-do-i-use-it/" id="bootstrap-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/bootstrap.png" style="responsive-image">
-        </div>
-        <div id="sass-tutorial" class="tutorials-cell">
+          <h3>Bootstrap</h3>
+        </a>
+        <a href="<?php echo site_url(); ?>/learn-sass-now/" id="sass-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/sass-150x150.png" style="responsive-image">
-        </div>
-        <div id="gulp-tutorial" class="tutorials-cell">
+          <h3>Sass</h3>
+        </a>
+        <a href="<?php echo site_url(); ?>/getting-started-with-gulp/" id="gulp-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/gulptut.png" style="responsive-image">
-        </div>
-        <div id="jekyll-tutorial" class="tutorials-cell">
+          <h3>Gulp</h3>
+        </a>
+        <a href="<?php echo site_url(); ?>/make-a-static-website-with-jekyll/" id="jekyll-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/jekylltut.png" style="responsive-image">
-        </div>
-        <div id="git-tutorial" class="tutorials-cell">
+          <h3>Jekyll</h3>
+        </a>
+        <a href="<?php echo site_url(); ?>/getting-started-with-git/" id="git-tutorial" class="tutorials-cell">
           <img src="<?php echo site_url(); ?>/wp-content/uploads/gittut.png" style="responsive-image">
-        </div>
+          <h3>Git</h3>
+        </a>
       </div>
-
     </section>
+
 
   </main>
 
