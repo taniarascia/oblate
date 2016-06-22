67959e68dc1a5d21fb8a3671ad9ad063a18818a7
Tania <taniarascia@gmail.com>
2016-06-22 16:21:40 -0500

    Front page

--- wp-content/themes/oblate/front-page.php
+++ wp-content/themes/oblate/front-page.php
@@ -5,6 +5,39 @@
     <!-- <section class="banner text-center">
     </section>-->
 
+    <section class="content no-padding-bottom">
+      <div class="small-container">
+        <h2 class="large-heading">Articles</h2>
+        <p>No ads. No sponsored posts. No fluff. Let's just learn and create.</p>
+        <a class="button" href="<?php echo site_url(); ?>/blog">View Blog</a>
+      </div>
+      <div class="large-container">
+        <div class="latest-articles text-center">
+          <?php 
+$args = array(
+'posts_per_page' => 3,
+);  
+$frontPage = new WP_Query( $args ); if ($frontPage->have_posts()) : while ($frontPage->have_posts()) : $frontPage->the_post();
+$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'thumbnail' );
+$url = $thumb['0']; ?>
+
+            <a href="<?php the_permalink(); ?>" class="article-link">
+          <article id="post-<?php the_ID(); ?>" class="article-excerpt">
+            <div class="article-header">
+              <h1><?php the_title(); ?></h1>
+                <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
+                <?php the_excerpt(); ?>
+            </div>
+            <div class="article-image">
+            <?php if ( get_post_thumbnail_id() ) { ?> <img src="<?php echo $url; ?>" alt="<?php the_title(); ?>"><?php } ?>
+            </div>
+          </article>
+        </a>
+            <?php endwhile; endif; ?>
+        </div>
+      </div>
+    </section>
+
     <section class="content">
       <div class="small-container">
         <h2 class="large-heading">Tutorials</h2>
@@ -51,61 +84,16 @@
           <img src="<?php echo site_url(); ?>/wp-content/uploads/grunt-150x150.png" class="responsive-image">
           <h3>Grunt</h3>
         </a>
-
-
       </div>
     </section>
 
-    <section class="content">
+    <section class="javascript content">
       <div class="small-container">
         <h2 class="large-heading">JavaScript</h2>
-        <p>Introductory lessons to JavaScript - Day by Day.</p>
+        <p>Introductory lessons on JavaScript - Day by Day.</p>
         <a class="button" href="<?php echo site_url(); ?>/javascript-day-one">Start Here</a>
       </div>
-    </section>
-
-    <a href="<?php echo site_url(); ?>/javascript-day-one">
-      <section class="javascript">
-        <img src="<?php echo site_url(); ?>/wp-content/uploads/jstut.png">
-      </section>
-    </a>
-
-    <section class="content no-padding-bottom">
-      <div class="small-container">
-        <h2 class="large-heading">Latest Articles</h2>
-        <p>No ads. No sponsored posts. No fluff. Let's just learn and create.</p>
-        <a class="button" href="<?php echo site_url(); ?>/blog">View Blog</a>
-      </div>
-      <div class="large-container">
-        <div class="latest-articles text-center">
-          <?php 
-$args = array(
-'posts_per_page' => 2,
-);  
-$frontPage = new WP_Query( $args ); if ($frontPage->have_posts()) : while ($frontPage->have_posts()) : $frontPage->the_post();
-$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'thumbnail' );
-$url = $thumb['0']; ?>
-
-            <a href="<?php the_permalink(); ?>" class="article-link">
-          <article id="post-<?php the_ID(); ?>" class="article-excerpt">
-
-            <div class="article-header">
-              <h1><?php the_title(); ?></h1>
-                <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
-                <?php the_excerpt(); ?>
-            </div>
-
-            <div class="article-image">
-            <?php if ( get_post_thumbnail_id() ) { ?> <img src="<?php echo $url; ?>" alt="<?php the_title(); ?>"><?php } ?>
-            </div>
-
-          </article>
-        </a>
-
-            <?php endwhile; endif; ?>
-
-        </div>
-      </div>
+      <img src="<?php echo site_url(); ?>/wp-content/uploads/jstut.png">
     </section>
 
   </main>
