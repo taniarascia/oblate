06f983fdd4b4f2ee581c0458ac57c36a45b179ff
Tania <taniarascia@gmail.com>
2016-07-28 14:20:39 -0500

    ME

--- wp-content/themes/oblate/content-page.php
+++ wp-content/themes/oblate/content-page.php
@@ -1,14 +1,3 @@
-<?php 
-$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'thumbnail' );
-$url = $thumb['0']; 
-?>
-
-<?php if ( get_post_thumbnail_id() ) { ?>
-	<div class="single-article-image">
-		<img src="<?php echo $url; ?>" alt="<?php the_title(); ?>">
-	</div>
-<?php } ?>
-
 <article id="post-<?php the_ID(); ?>">
 
 	<div class="single-article-header">
