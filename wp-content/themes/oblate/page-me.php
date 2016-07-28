067a62036a0a98ce10e47f3965bf403620dc79b9
Tania <taniarascia@gmail.com>
2016-07-28 14:22:03 -0500

    ME

--- wp-content/themes/oblate/page-me.php
+++ wp-content/themes/oblate/page-me.php
@@ -1,33 +1,37 @@
 <?php get_header(); ?>
 
-	<main class="main-content">
-
-		<div class="small-container">
-
-			<?php 
+	<?php 
 $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'full' );
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID), 'thumbnail' );
 $img = $image['0']; 
 ?>
 
-				<?php if ( get_post_thumbnail_id() ) { ?>
-					<div class="single-article-image">
-						<img src="<?php echo $img; ?>" alt="<?php the_title(); ?>" class="responsive-image">
+		<main class="main-content">
+
+			<?php if ( get_post_thumbnail_id() ) { ?>
+				<div class="single-article-image">
+					<img src="<?php echo $img; ?>" alt="<?php the_title(); ?>">
+				</div>
+				<?php } ?>
+
+					<div class="small-container">
+
+						<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+							<article id="post-<?php the_ID(); ?>">
+
+								<div class="single-article-header">
+									<h1 class="large-heading"><?php the_title(); ?></h1>
+								</div>
+
+								<?php the_content(); ?>
+
+							</article>
+
+							<?php endwhile; endif; ?>
+
 					</div>
-					<?php } ?>
 
-						<article id="post-<?php the_ID(); ?>">
+		</main>
 
-							<div class="single-article-header">
-								<h1 class="large-heading"><?php the_title(); ?></h1>
-							</div>
-
-							<?php the_content(); ?>
-
-						</article>
-
-		</div>
-
-	</main>
-
-	<?php get_footer(); ?>
+		<?php get_footer(); ?>
