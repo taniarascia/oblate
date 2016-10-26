5898698362e2b2acbae1fea1dd19f98ecbdf1fa7
Tania <taniarascia@gmail.com>
2016-10-26 11:23:59 -0500

    Music project

--- wp-content/themes/oblate/page-contact.php
+++ wp-content/themes/oblate/page-contact.php
@@ -10,17 +10,6 @@
 
 						endwhile; endif; ?>
 
-        <div class="text-center margin-top">
-          <p>No cute bullshit about buying me pizza or coffee. If you've found my website useful and would like to press that button, I appreciate it!</p>
-
-          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_s-xclick" />
-            <input type="hidden" name="hosted_button_id" value="7JMKA39JXQNGL" />
-            <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate" />
-            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-          </form>
-        </div>
-
     </div>
 
 
--- wp-content/themes/oblate/page-projects.php
+++ wp-content/themes/oblate/page-projects.php
@@ -1,49 +1,61 @@
 <?php get_header(); ?>
 
-  <main class="main-content">
+	<main class="main-content">
 
-    <section id="primitive">
-      <div class="container">
-        <h1>Primitive</h1>
-        <p>A Sass Starter-pack for Front End Web Developers. Also includes <span class="eighties">Eighties Mode!</span></p>
-        <a class="button-primitive" href="http://taniarascia.github.io/primitive/" target="_blank">View Project</a>
-      </div>
-    </section>
+		<section id="primitive">
+			<div class="large-container">
+				<h1>Primitive</h1>
+				<p>A Sass Starter-pack for Front End Web Developers. Also includes <span class="eighties">Eighties Mode!</span></p>
+				<a class="button-primitive" href="http://taniarascia.github.io/primitive/" target="_blank">View Project</a>
+				<a class="button-primitive" href="https://github.com/taniarascia/primitive/" target="_blank">GitHub Repo</a>
+			</div>
+		</section>
 
-    <section id="new-moon">
-      <div class="container">
-        <img src="https://www.taniarascia.com/wp-content/uploads/newmoon.png" class="responsive-image">
-        <h1>New Moon</h1>
-        <p>Beautiful syntax highlighting for Atom, Brackets, &amp; Sublime.</p>
-        <a class="button" target="_blank" href="http://taniarascia.github.io/new-moon/">View Project</a>
-      </div>
-    </section>
+		<section id="new-moon">
+			<div class="large-container">
+				<img src="https://www.taniarascia.com/wp-content/uploads/newmoon.png" class="responsive-image">
+				<h1>New Moon</h1>
+				<p>Beautiful syntax highlighting for Atom, Brackets, &amp; Sublime.</p>
+				<a class="button" target="_blank" href="http://taniarascia.github.io/new-moon/">View Project</a>
+				<a class="button" target="_blank" href="https://github.com/taniarascia/new-moon">GitHub Repo</a>
+			</div>
+		</section>
 
-    <section id="more">
-      <div class="container">
-        <h1>More Projects</h1>
-        <div class="flex-row">
-          <div class="flex-column">
-            <div class="repo">
-              <h3><a href="https://github.com/taniarascia/wp-functions" target="_blank">Useful WordPress Functions</a></h3>
-              <p>Useful functions to enhance and clean up a WordPress theme.</p>
-            </div>
-          </div>
-          <div class="flex-column">
-            <div class="repo">
-              <h3><a href="https://taniarascia.github.io/html5-test/" target="_blank">HTML5 Test Page</a></h3>
-              <p>Common HTML5 elements for CSS testing purposes, with comparisons for Bootstrap, Foundation, Primitive, Skeleton, and Material.</p>
-            </div>
-          </div>
-          <div class="flex-column">
-            <div class="repo">
-              <h3><a href="https://github.com/taniarascia/fledge" target="_blank">Fledge</a></h3>
-              <p>Clean, minimalist theme for phpBB.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+		<section id="music">
+			<div class="large-container">
+				<div class="music-container">
+					<h1>Music</h1>
+					<p>Really weird musical side project.</p>
+					<a class="button" target="_blank" href="https://www.taniarascia.com/music/">View Project</a>
+				</div>
+			</div>
+		</section>
 
-  <?php get_footer(); ?>
+		<section id="more">
+			<div class="large-container">
+				<h1>More Projects</h1>
+				<div class="flex-row">
+					<div class="flex-column">
+						<div class="repo">
+							<h3><a href="https://github.com/taniarascia/wp-functions" target="_blank">Useful WordPress Functions</a></h3>
+							<p>Useful functions to enhance and clean up a WordPress theme.</p>
+						</div>
+					</div>
+					<div class="flex-column">
+						<div class="repo">
+							<h3><a href="https://taniarascia.github.io/html5-test/" target="_blank">HTML5 Test Page</a></h3>
+							<p>Common HTML5 elements for CSS testing purposes, with comparisons for popular frameworks.</p>
+						</div>
+					</div>
+					<div class="flex-column">
+						<div class="repo">
+							<h3><a href="https://github.com/taniarascia/fledge" target="_blank">Fledge</a></h3>
+							<p>Clean, minimalist theme for phpBB.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<?php get_footer(); ?>
