a88daf0c7cb6c20ad29c513b3e272b4be8c700e0
Tania <taniarascia@gmail.com>
2016-11-01 15:42:02 -0500

    Alt

--- wp-content/themes/oblate/front-page.php
+++ wp-content/themes/oblate/front-page.php
@@ -52,39 +52,39 @@ $url = $thumb['0']; ?>
 		<section class="tutorials">
 			<div class="tutorials-grid">
 				<a href="<?php echo site_url(); ?>/developing-a-wordpress-theme-from-scratch/" id="wordpress-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/wptut.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/wptut.png" class="responsive-image" alt="WordPress">
           <h3>WordPress</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/what-is-bootstrap-and-how-do-i-use-it/" id="bootstrap-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/bootstrap.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/bootstrap.png" class="responsive-image" alt="Bootstrap">
           <h3>Bootstrap</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/learn-sass-now/" id="sass-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/sass-150x150.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/sass-150x150.png" class="responsive-image" alt="Sass">
           <h3>Sass</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/getting-started-with-gulp/" id="gulp-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/gulptut.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/gulptut.png" class="responsive-image" alt="Gulp">
           <h3>Gulp</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/make-a-static-website-with-jekyll/" id="jekyll-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/jekylltut.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/jekylltut.png" class="responsive-image" alt="Jekyll">
           <h3>Jekyll</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/getting-started-with-git/" id="git-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/gittut.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/gittut.png" class="responsive-image" alt="Git">
           <h3>Git</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/getting-started-with-aws-setting-up-a-virtual-server/" id="aws-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/awstut.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/awstut.png" class="responsive-image" alt="AWS">
           <h3>Amazon Web Services</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/local-environment/" id="apache-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/apache.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/apache.png" class="responsive-image" alt="Apache">
           <h3>Apache</h3>
         </a>
 				<a href="<?php echo site_url(); ?>/getting-started-with-grunt-and-sass/" id="grunt-tutorial" class="tutorials-cell">
-          <img src="<?php echo site_url(); ?>/wp-content/uploads/grunt-150x150.png" class="responsive-image">
+          <img src="<?php echo site_url(); ?>/wp-content/uploads/grunt-150x150.png" class="responsive-image" alt="Grunt">
           <h3>Grunt</h3>
         </a>
 			</div>
@@ -96,7 +96,7 @@ $url = $thumb['0']; ?>
 				<p>Introductory lessons on JavaScript - Day by Day.</p>
 				<a class="button" href="<?php echo site_url(); ?>/javascript-day-one">Start Here</a>
 			</div>
-			<img src="<?php echo site_url(); ?>/wp-content/uploads/jstut.png">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/jstut.png" alt="JavaScript">
 		</section>
 
 	</main>
