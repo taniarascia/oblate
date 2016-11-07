7c27645735f3c4846b5f4236a859ab46e4eb4778
Tania <taniarascia@gmail.com>
2016-11-07 16:50:33 -0600

    Functions

--- wp-content/themes/oblate/functions.php
+++ wp-content/themes/oblate/functions.php
@@ -170,38 +170,39 @@ add_filter( 'excerpt_save_pre', 'filterCode', 9 );
 add_filter( 'w3tc_can_print_comment', function( $w3tc_setting ) { return false; }, 10, 1 );
 
 // Create 1 Custom Post type for a Demo, called HTML5-Blank
-function create_post_portfolio()
-{
-    register_post_type('portfolio-items', // Register Custom Post Type
-        array(
-        'labels' => array(
-            'name' => __('Portfolio Item', 'oblate'), // Rename these to suit
-            'singular_name' => __('Portfolio Item', 'oblate'),
-            'add_new' => __('Add New', 'oblate'),
-            'add_new_item' => __('Add New Portfolio Item', 'oblate'),
-            'edit' => __('Edit', 'oblate'),
-            'edit_item' => __('Edit Portfolio Item', 'oblate'),
-            'new_item' => __('New Portfolio Item', 'oblate'),
-            'view' => __('View Portfolio Item', 'oblate'),
-            'view_item' => __('View Portfolio Item', 'oblate'),
-            'search_items' => __('Search Portfolio Item', 'oblate'),
-            'not_found' => __('No Portfolio Items found', 'oblate'),
-            'not_found_in_trash' => __('No Portfolio Items found in Trash', 'oblate')
-        ),
-        'public' => true,
-        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-        'has_archive' => true,
-        'supports' => array(
-            'title',
-            'editor',
-            'excerpt',
-            'thumbnail'
-        ), // Go to Dashboard Custom HTML5 Blank post for supports
-        'can_export' => true, // Allows export in Tools > Export
-        'taxonomies' => array(
-            'post_tag',
-            'category'
-        ) // Add Category and Post Tags support
-    ));
+function create_post_portfolio() {
+	register_post_type('portfolio-items', // Register Custom Post Type
+		array(
+		'labels' => array(
+			'name' => __('Portfolio Item', 'oblate'), // Rename these to suit
+			'singular_name' => __('Portfolio Item', 'oblate'),
+			'add_new' => __('Add New', 'oblate'),
+			'add_new_item' => __('Add New Portfolio Item', 'oblate'),
+			'edit' => __('Edit', 'oblate'),
+			'edit_item' => __('Edit Portfolio Item', 'oblate'),
+			'new_item' => __('New Portfolio Item', 'oblate'),
+			'view' => __('View Portfolio Item', 'oblate'),
+			'view_item' => __('View Portfolio Item', 'oblate'),
+			'search_items' => __('Search Portfolio Item', 'oblate'),
+			'not_found' => __('No Portfolio Items found', 'oblate'),
+			'not_found_in_trash' => __('No Portfolio Items found in Trash', 'oblate')
+		),
+		'public' => true,
+		'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+		'has_archive' => true,
+		'supports' => array(
+			'title',
+			'editor',
+			'excerpt',
+			'thumbnail'
+		), // Go to Dashboard Custom HTML5 Blank post for supports
+		'can_export' => true, // Allows export in Tools > Export
+		'taxonomies' => array(
+				'post_tag',
+				'category'
+		) // Add Category and Post Tags support
+	));
 }
 add_action('init', 'create_post_portfolio'); // Add our Portfolio Item Type
+
+
