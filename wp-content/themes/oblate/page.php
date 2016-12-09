cec331c1894163a8b6b43c6082f585af62a091e1
Tania <taniarascia@gmail.com>
2016-12-09 16:57:55 -0600

    Subtle

--- wp-content/themes/oblate/page.php
+++ wp-content/themes/oblate/page.php
@@ -1,17 +1,17 @@
 <?php get_header(); ?>
 
-  <main class="main-content">
+	<main class="main-content">
+		<section class="single">
+			<div class="small-container">
 
-      <div class="small-container">
-
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
 						get_template_part( 'content-page', get_post_format() );
 
 						endwhile; endif; ?>
 
-      </div>
+			</div>
+		</section>
+	</main>
 
-  </main>
-
-  <?php get_footer(); ?>
+	<?php get_footer(); ?>
