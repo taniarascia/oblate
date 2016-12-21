e89996f50e46368b3a0754669fdca530d58cbd3f
Tania <taniarascia@gmail.com>
2016-12-21 11:41:04 -0600

    Contact page

--- wp-content/themes/oblate/page-contact.php
+++ wp-content/themes/oblate/page-contact.php
@@ -1,13 +1,15 @@
 <?php get_header(); ?>
 
-<div class="small-container">
+<section class="page">
+	<div class="small-container">
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
-		get_template_part( 'content-page', get_post_format() );
+			get_template_part( 'content-page', get_post_format() );
 
-	endwhile; endif; ?>
+		endwhile; endif; ?>
 
-</div>
+	</div>
+</section>
 
 <?php get_footer(); ?>
