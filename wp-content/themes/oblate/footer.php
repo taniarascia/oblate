d3868b02a26d4afd60c52bf846785c7f2cede649
Tania <taniarascia@gmail.com>
2016-12-27 13:25:29 -0600

    Fix codepen link

--- wp-content/themes/oblate/footer.php
+++ wp-content/themes/oblate/footer.php
@@ -4,7 +4,7 @@
 	<a href="<?php echo site_url(); ?>/feed" class="social-icon"><i class="fa fa-rss fa-2x"></i></a>
 	<a href="https://twitter.com/taniarascia" class="social-icon"><i class="fa fa-twitter fa-2x"></i></a>
 	<a href="https://github.com/taniarascia" class="social-icon"><i class="fa fa-github fa-2x"></i></a>
-	<a href="https://codepen.com/taniarascia" class="social-icon"><i class="fa fa-codepen fa-2x"></i></a>
+	<a href="https://codepen.io/taniarascia/" class="social-icon"><i class="fa fa-codepen fa-2x"></i></a>
 	<a href="https://stackoverflow.com/users/4541434/tania-rascia" class="social-icon"><i class="fa fa-stack-overflow fa-2x"></i></a>
 </footer>
 
