5e5241f2da060d43a205b8d5a353914008b7d27d
Tania <taniarascia@gmail.com>
2017-01-06 13:14:59 -0600

    Version

--- wp-content/themes/oblate/index.php
+++ wp-content/themes/oblate/index.php
@@ -3,13 +3,13 @@
 <section class="blog-index">
 
 	<?php get_search_form(); ?>
-
+	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
 		get_template_part( 'content', get_post_format() );
 
 	endwhile; ?>
-
+	
 		<div class="posts-links">
 			<div class="pagination-left">
 				<?php previous_posts_link(); ?>
