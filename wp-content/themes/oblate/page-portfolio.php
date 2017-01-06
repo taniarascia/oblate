6c2262fe33e72b5fa4accd82547d01b99e2496fd
Tania <taniarascia@gmail.com>
2017-01-06 10:21:49 -0600

    Portfolio

--- wp-content/themes/oblate/page-portfolio.php
+++ wp-content/themes/oblate/page-portfolio.php
@@ -10,17 +10,9 @@
 
 			if ( $portfolio->have_posts() ) :  while ( $portfolio->have_posts() ) : $portfolio->the_post(); ?>
 
-<section class="portfolio-container">
-	<div class="portfolio-description">
-		<div class="portfolio-inner">
-			<h3><?php the_title(); ?></h3>
-			<a class="button" href="<?php echo get_the_excerpt(); ?>" target="_blank">View Website</a>
-		</div>
-	</div>
 	<div class="portfolio-example vertical-center">
 		<a href="<?php echo get_the_excerpt(); ?>" target="_blank"><img src="<?php echo the_post_thumbnail_url( 'large' ); ?>" alt="<?php the_title(); ?>"></a>
 	</div>
-</section>
 
 <?php endwhile; endif; wp_reset_postdata(); ?>
 
