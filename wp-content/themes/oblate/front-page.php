bd80f1fc4df90f61d54f83cd3c1aac969f46262a
Tania <taniarascia@gmail.com>
2017-01-17 10:08:14 -0600

    Add new tutorials

--- wp-content/themes/oblate/front-page.php
+++ wp-content/themes/oblate/front-page.php
@@ -49,44 +49,56 @@ if ( $latest->have_posts() ) : ?>
 	</section>
 
 <section class="tutorials">
-<div class="tutorials-grid">
-<a href="<?php echo site_url(); ?>/developing-a-wordpress-theme-from-scratch/" id="wordpress-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/wptut.png" class="responsive-image" alt="WordPress">
-<h3>WordPress</h3>
-</a>
-<a href="<?php echo site_url(); ?>/what-is-bootstrap-and-how-do-i-use-it/" id="bootstrap-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/bootstrap.png" class="responsive-image" alt="Bootstrap">
-<h3>Bootstrap</h3>
-</a>
-<a href="<?php echo site_url(); ?>/learn-sass-now/" id="sass-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/sass-150x150.png" class="responsive-image" alt="Sass">
-<h3>Sass</h3>
-</a>
-<a href="<?php echo site_url(); ?>/getting-started-with-gulp/" id="gulp-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/gulptut.png" class="responsive-image" alt="Gulp">
-<h3>Gulp</h3>
-</a>
-<a href="<?php echo site_url(); ?>/make-a-static-website-with-jekyll/" id="jekyll-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/jekylltut.png" class="responsive-image" alt="Jekyll">
-<h3>Jekyll</h3>
-</a>
-<a href="<?php echo site_url(); ?>/getting-started-with-git/" id="git-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/gittut.png" class="responsive-image" alt="Git">
-<h3>Git</h3>
-</a>
-<a href="<?php echo site_url(); ?>/getting-started-with-aws-setting-up-a-virtual-server/" id="aws-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/awstut.png" class="responsive-image" alt="AWS">
-<h3>Amazon Web Services</h3>
-</a>
-<a href="<?php echo site_url(); ?>/local-environment/" id="lamp-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/mamppro.png" class="responsive-image" alt="Apache">
-<h3>LAMP Stack</h3>
-</a>
-<a href="<?php echo site_url(); ?>/getting-started-with-grunt-and-sass/" id="grunt-tutorial" class="tutorials-cell">
-<img src="<?php echo site_url(); ?>/wp-content/uploads/grunt-150x150.png" class="responsive-image" alt="Grunt">
-<h3>Grunt</h3>
-</a>
-</div>
+	<div class="tutorials-grid">
+		<a href="<?php echo site_url(); ?>/developing-a-wordpress-theme-from-scratch/" id="wordpress-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/wptut.png" class="responsive-image" alt="WordPress">
+			<h3>WordPress</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/what-is-bootstrap-and-how-do-i-use-it/" id="bootstrap-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/bootstrap.png" class="responsive-image" alt="Bootstrap">
+			<h3>Bootstrap</h3>
+			</a>
+		<a href="<?php echo site_url(); ?>/learn-sass-now/" id="sass-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/sass-150x150.png" class="responsive-image" alt="Sass">
+			<h3>Sass</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/getting-started-with-gulp/" id="gulp-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/gulptut.png" class="responsive-image" alt="Gulp">
+			<h3>Gulp</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/make-a-static-website-with-jekyll/" id="jekyll-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/jekylltut.png" class="responsive-image" alt="Jekyll">
+			<h3>Jekyll</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/getting-started-with-git/" id="git-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/gittut.png" class="responsive-image" alt="Git">
+			<h3>Git</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/how-to-use-the-command-line-for-apple-macos-and-linux/" id="shell-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/term.png" class="responsive-image" alt="Command Line">
+			<h3>Command Line</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/javascript-day-one" id="js-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/js-150x150.png" class="responsive-image" alt="JavaScript">
+			<h3>JavaScript</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/getting-started-with-aws-setting-up-a-virtual-server/" id="aws-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/awstut.png" class="responsive-image" alt="AWS">
+			<h3>Amazon Web Services</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/getting-started-with-grunt-and-sass/" id="grunt-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/grunt-150x150.png" class="responsive-image" alt="Grunt">
+			<h3>Grunt</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/local-environment/" id="lamp-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/mamppro.png" class="responsive-image" alt="Apache">
+			<h3>LAMP Stack</h3>
+		</a>
+		<a href="<?php echo site_url(); ?>/javascript-day-one" id="apple-tutorial" class="tutorials-cell">
+			<img src="<?php echo site_url(); ?>/wp-content/uploads/appletut-150x150.png" class="responsive-image" alt="Apple">
+			<h3>macOS</h3>
+		</a>
+	</div>
 </section>
 
 <?php get_footer(); ?>
