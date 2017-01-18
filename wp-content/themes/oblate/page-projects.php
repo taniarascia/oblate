e384370521f5e016cca0b8e3bebd071943a536cb
Tania Rascia <taniarascia@gmail.com>
2017-01-17 22:22:05 -0600

    Projects page updates

--- wp-content/themes/oblate/page-projects.php
+++ wp-content/themes/oblate/page-projects.php
@@ -4,7 +4,7 @@
 	<div class="small-container">
 		<h1>Primitive</h1>
 		<p>A minimalist CSS framework and Sass starter-pack for front end web developers. Also includes <span class="eighties">Eighties Mode!</span></p>
-		<a class="button-primitive" href="http://taniarascia.github.io/primitive/" target="_blank">View Project</a>
+		<a class="button-primitive" href="https://taniarascia.github.io/primitive/" target="_blank">View Project</a>
 		<a class="button-primitive" href="http://github.com/taniarascia/primitive/" target="_blank">GitHub Repo</a>
 	</div>
 </section>
@@ -14,7 +14,7 @@
 		<img src="<?php echo site_url(); ?>/wp-content/uploads/newmoon.png" class="responsive-image">
 		<h1>New Moon</h1>
 		<p>Beautiful syntax highlighting for Atom, Brackets, &amp; Sublime.</p>
-		<a class="button" target="_blank" href="http://taniarascia.github.io/new-moon/">View Project</a>
+		<a class="button" target="_blank" href="https://taniarascia.github.io/new-moon/">View Project</a>
 		<a class="button" target="_blank" href="https://github.com/taniarascia/new-moon">GitHub Repo</a>
 	</div>
 </section>
