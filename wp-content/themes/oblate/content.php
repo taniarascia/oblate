b47096c18bbc0fdfa7c8b480168bb840cd8e7791
Tania <taniarascia@gmail.com>
2017-02-15 10:10:26 -0600

    Move time above titles

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -3,8 +3,8 @@
 		<?php if ( get_post_thumbnail_id() ) { ?><a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>" height="150" width="150"></a><?php } ?>
 	</div>
 	<div>
+	<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-			<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
 			<?php the_excerpt(); ?>
 	</div>
 </article>
