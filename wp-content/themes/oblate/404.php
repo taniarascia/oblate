eef9314dbd17369124733ed643818c8caaa145e3
Tania <taniarascia@gmail.com>
2017-02-28 10:08:01 -0600

    BLUE SCREEN OF DEATH

--- wp-content/themes/oblate/404.php
+++ wp-content/themes/oblate/404.php
@@ -1,10 +1,21 @@
 <?php get_header();?>
+		
+	<section class="four-oh-four">
+		
+		<div class="container">
 
-		<article id="post-<?php the_ID(); ?>">
-			<div class="page-header text-center">
-				<h1>404</h1>
-			</div>
-			<p class="text-center">Page not found.</p>
-		</article>
+			<article id="post-<?php the_ID(); ?>">
+				<div class="page-header text-center">
+					<h1><span>404</span></h1>
+				</div>
+				<p>A fatal exception 0E has occurred at 74616e6961 in 404: page not found.</p>
+				<p class="bsod-list">* &nbsp; Click any link to terminate the current application.</p>
+				<p class="bsod-list">* &nbsp; Press UP + UP + DOWN + DOWN + LEFT + RIGHT + LEFT + RIGHT + B + A at any time to improve the situation.</p>
+				<p class="text-right">Click any link to continue _</p>
+			</article>
+		
+		</div>
+
+	</section>
 
 <?php get_footer();?>
