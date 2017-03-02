4c29c8c286fd3107b6336cca80ba325c1a1456c8
Tania Rascia <taniarascia@gmail.com>
2017-03-01 22:29:07 -0600

    Lets see what happens

--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -4,7 +4,6 @@
 	</div>
 	<div>
 	<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
-	<?php the_tags('<div class="tags-container-page"><span class="tags">' ,'</span><span class="tags">','</span></div>'); ?>
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 			<?php the_excerpt(); ?>
 	</div>
