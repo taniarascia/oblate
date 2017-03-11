f217d057c6e73fda1c3f4ed075bc83f49bcf9598
Tania Rascia <taniarascia@gmail.com>
2017-03-11 14:54:36 -0600

    Left aligning

--- wp-content/themes/oblate/index.php
+++ wp-content/themes/oblate/index.php
@@ -2,8 +2,6 @@
 
 <section class="blog-index">
 
-	<?php get_search_form(); ?>
-	
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
 
 		get_template_part( 'content', get_post_format() );
