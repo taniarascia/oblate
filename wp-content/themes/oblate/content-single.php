fddc157af366ab777d1253f34bf385185be1d88d
Tania Rascia <taniarascia@gmail.com>
2017-03-11 22:24:51 -0600

    Nevermind, center align it

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -1,13 +1,8 @@
 <article id="post-<?php the_ID(); ?>">
 		
 	<div class="single-article-header">
-		<div class="author">
-			<img src="<?php echo site_url(); ?>/wp-content/themes/oblate/images/tr.png"> 
-			<div class="name"><a href="<?php echo site_url(); ?>/me">Tania Rascia</a> - <a href="https://twitter.com/taniarascia" target="_blank"><i class="fa fa-twitter"></i></a> <a href="https://github.com/taniarascia" target="_blank"><i class="fa fa-github"></i></a> </div>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time( 'F j, Y' ); ?> - <?php comments_popup_link( 'Leave a response', '1 response', '% responses' ); ?></time>
-		</div>
 		
-	<h1><?php the_title(); ?></h1>
+	<h1 class="text-center"><?php the_title(); ?></h1>
 		<?php if ( has_excerpt() ) { ?>
 		<div class="lead-excerpt"><?php the_excerpt(); ?></div>
 		<?php } ?>
--- wp-content/themes/oblate/front-page.php
+++ wp-content/themes/oblate/front-page.php
@@ -8,28 +8,10 @@
 		<div class="container">
 			<h1>Tania Rascia</h1>
 			<?php the_content(); ?>
-				<h1 class="secondary">Latest Articles</h1>
 		</div>
 	</div>
 	
 	<?php endwhile; endif; ?>
-
-	<div class="latest">
-	<?php 
-	$args = array(
-						'posts_per_page' => '3',
-					);
-
-	$latest = new WP_Query( $args );
-
-	if ( $latest->have_posts() ) : ?>
-
-
-	<?php while ( $latest->have_posts() ) : $latest->the_post(); ?>
-		<?php get_template_part( 'content-front', get_post_format() ); ?>
-	<?php endwhile; ?>
-<?php endif; wp_reset_postdata(); ?>
-	</div>
 
 </section>
 
