ef758ff8b25024d8a006504ec1326d2f1cd987c6
Tania Rascia <taniarascia@gmail.com>
2017-03-11 22:42:23 -0600

    Reverting

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -1,16 +1,27 @@
 <article id="post-<?php the_ID(); ?>">
-		
+
 	<div class="single-article-header">
-	
+
 		<div class="single-article-image">
-			<?php if ( get_post_thumbnail_id() ) { ?><a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>" height="150" width="150"></a><?php } ?>
+			<?php if ( get_post_thumbnail_id() ) { ?><a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>" height="150" width="150"></a>
+				<?php } ?>
 		</div>
 
 		<h1 class="text-center"><?php the_title(); ?></h1>
-			<?php if ( has_excerpt() ) { ?>
-			<div class="lead-excerpt"><?php the_excerpt(); ?></div>
+		<?php if ( has_excerpt() ) { ?>
+			<div class="lead-excerpt">
+				<?php the_excerpt(); ?>
+			</div>
 			<?php } ?>
 
+	</div>
+
+	<img src="<?php echo site_url(); ?>/wp-content/themes/oblate/images/tr.png">
+	<div class="name"><a href="<?php echo site_url(); ?>/me">Tania Rascia</a> - <a href="https://twitter.com/taniarascia" target="_blank"><i class="fa fa-twitter"></i></a> <a href="https://github.com/taniarascia" target="_blank"><i class="fa fa-github"></i></a> </div>
+	<time datetime="<?php the_time( 'Y-m-d' ); ?>">
+		<?php the_time( 'F j, Y' ); ?> -
+			<?php comments_popup_link( 'Leave a response', '1 response', '% responses' ); ?>
+	</time>
 	</div>
 
 	<?php the_content(); ?>
