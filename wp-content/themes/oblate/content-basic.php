163d3e901e66113e613bf543a94b8ef09fb0f1a4
Tania <taniarascia@gmail.com>
2017-03-23 17:01:25 -0500

    Changing the front page and adding images on mobile

--- wp-content/themes/oblate/content-basic.php
+++ wp-content/themes/oblate/content-basic.php
@@ -1,10 +1,17 @@
-<article id="post-<?php the_ID(); ?>" class="article-excerpt">
-	<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
-	<h1>
-		<a href="<?php the_permalink(); ?>">
-			<?php the_title(); ?>
-		</a>
-	</h1>
-	<?php the_excerpt(); ?>
-	<?php the_tags( '<div class="tags">', '', '</div>' ); ?>
+<article id="post-<?php the_ID(); ?>" class="article-excerpt article-flex">
+	<div>
+		<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
+		<h1>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+		<?php the_excerpt(); ?>
+		<?php the_tags( '<div class="tags">', '', '</div>' ); ?>
+	</div>
+	<?php if ( get_post_thumbnail_id() ) { ?>
+	<div class="basic-image">
+		<a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>" height="150" width="150"></a>
+	</div>
+	<?php } ?>
 </article>
--- wp-content/themes/oblate/content.php
+++ wp-content/themes/oblate/content.php
@@ -3,6 +3,7 @@
 	<div class="article-image">
 		<a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" alt="<?php the_title(); ?>" height="150" width="150"></a>
 	</div>
+	<div>
 	<?php } ?>
 	<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F j, Y'); ?></time>
 	<h1>
@@ -12,4 +13,5 @@
 	</h1>
 	<?php the_excerpt(); ?>
 	<?php the_tags( '<div class="tags tags-container">', '', '</div>' ); ?>
+	</div>
 </article>
