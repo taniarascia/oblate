97737afc8953e2392d2981e8a9928906cef9794e
Tania <taniarascia@gmail.com>
2017-04-03 17:28:57 -0500

    Resume

---
+++ wp-content/themes/oblate/page-resume.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+<div class="container">
+	<?php the_content(); ?>
+</div>
+
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
