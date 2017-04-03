1861c3e7058e77b93cf06adc840151446b5798ee
Tania <taniarascia@gmail.com>
2017-04-03 15:58:04 -0500

    Uh

--- wp-content/themes/oblate/searchform.php
+++ wp-content/themes/oblate/searchform.php
@@ -1,7 +1,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'search for anything' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'search for anything', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 	</label>
 	<button type="submit" class="search-submit"><i class="fa fa-search" aria-hidden="true"></i><span class="screen-reader-text">Search</span></button>
 </form>
