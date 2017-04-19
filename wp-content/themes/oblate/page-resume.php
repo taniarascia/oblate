68e1109fe349becebedef6c4f5b7323a10923702
Tania Rascia <taniarascia@gmail.com>
2017-04-19 13:30:47 -0500

    Box shadow

--- wp-content/themes/oblate/page-resume.php
+++ wp-content/themes/oblate/page-resume.php
@@ -2,11 +2,9 @@
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-<section class="resume-outer">
 	<div class="container">
 		<?php the_content(); ?>
 	</div>
-</section>
 
 <?php endwhile; endif; ?>
 
