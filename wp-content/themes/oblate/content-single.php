593b6c9088ee6fc4e811379a862b8e439e1bcfcb
Tania Rascia <taniarascia@gmail.com>
2017-05-16 09:59:50 -0500

    Signups

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -51,7 +51,6 @@
 								<div class="response" id="mce-error-response" style="display:none"></div>
 								<div class="response" id="mce-success-response" style="display:none"></div>
 							</div>
-							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ec794fab6e35a491a001cc25d_5276386071" tabindex="-1" value=""></div>
 							<div class="clear"><input type="submit" value="Join" name="subscribe" id="mc-embedded-subscribe" class="button alt-button"></div>
 						</form>
