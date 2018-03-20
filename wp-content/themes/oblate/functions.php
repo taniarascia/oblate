8c411ad8dd02917f8149a2432bf0dead94ee9014
Tania <taniarascia@gmail.com>
2018-03-20 14:35:41 +0000

    This won't fix it

--- wp-content/themes/oblate/functions.php
+++ wp-content/themes/oblate/functions.php
@@ -90,7 +90,8 @@ add_action( 'wp_enqueue_scripts', 'starter_scripts' );
 
 function oblate_scripts() {
 	wp_dequeue_style( 'starter-style' );
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/main.css?3-14-2018' );
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/css/main.css?3-20-2018' 
+);
 	wp_enqueue_script( 'prism', get_template_directory_uri() . '/js/prism.js', array(), '1.0.0', true );
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js?06-06-2017', array(), '1.0.0', true );
 }
