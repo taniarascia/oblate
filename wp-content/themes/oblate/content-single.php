a1ba1eca7311568ca309fc93b10cd6ee751b69ff
Tania Rascia <taniarascia@gmail.com>
2018-07-19 15:14:50 -0500

    Quality and morality

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -15,7 +15,7 @@
 						<?php the_time( 'F j, Y' ); ?></time>
 					</div>	
 			<div class="single-social">
-			<p>Liked this article? <a href="http://ko-fi.com/taniarascia">Buy me a coffee</a>.</p>
+			<p>Liked this article? <a href="https://ko-fi.com/taniarascia">Buy me a coffee</a>.</p>
 			</div>
 		</div>
 	</header>
@@ -29,12 +29,12 @@
 				<h2 class="text-center">Quality and morality on the web</h2>
 				<p>Hi, my name is <strong>Tania Rascia</strong>. I have turned down every advertiser, sponsored post, and affiliate who has come to me. <strong>I give away all my knowledge for free to my <mark>300,000+ monthly readers</mark></strong> and helped thousands of people learn and successfully transition into a design/development career.</p>
 
-				<p><strong>You know what I stand for, and you know my commitment to you - no bullshit, no sponsored posts, no pop-ups, no blocked content, schemes, tactics, or gimmicks. Just free, quality content. <mark>Support me by buying me a coffee</marK></strong></p>
+				<p><strong>You know what I stand for, and you know my commitment to you - no bullshit, no sponsored posts, no pop-ups, no blocked content, ads, schemes, tactics, or gimmicks. Just free, quality content. <mark>Support me by buying me a coffee</mark></strong>.</p>
 				
 				<div class="author-options">
 					<div>
-					<h3>I really appreciate your support!</h3>
-					<p><a class="button paypal-button" href="https://ko-fi.com/taniarascia" target="_blank">Buy me a coffee</a> <a class="button paypal-button" href="https://patreon.com/taniarascia" target="_blank">Be a patron</a></p>
+						<h3>I really appreciate your support!</h3>
+						<p><a class="button paypal-button" href="https://ko-fi.com/taniarascia" target="_blank">Buy me a coffee</a> <a class="button paypal-button" href="https://patreon.com/taniarascia" target="_blank">Be a patron</a></p>
 					</div>
 				</div>
 			</div>
--- wp-content/themes/oblate/front-page.php
+++ wp-content/themes/oblate/front-page.php
@@ -21,6 +21,24 @@
 
 <?php endwhile; endif; ?>
 
+<section>
+	<div class="small-container">
+		<div class="author">
+			<h2 class="text-center">Quality and morality on the web</h2>
+			<p>I have turned down every advertiser, sponsored post, and affiliate who has come to me. <strong>I give away all my knowledge for free to my <mark>300,000+ monthly readers</mark></strong> and helped thousands of people learn and successfully transition into a design/development career.</p>
+
+			<p><strong>You know what I stand for, and you know my commitment to you - no bullshit, no sponsored posts, no pop-ups, no blocked content, ads, schemes, tactics, or gimmicks. Just free, quality content. <mark>Support me by buying me a coffee</mark></strong>.</p>
+			
+			<div class="author-options">
+				<div>
+					<h3>I really appreciate your support!</h3>
+					<p><a class="button paypal-button" href="https://ko-fi.com/taniarascia" target="_blank">Buy me a coffee</a> <a class="button paypal-button" href="https://patreon.com/taniarascia" target="_blank">Be a patron</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
 <section class="quote-container">
 	<div class="small-container">
 		<blockquote class="testimonial">"Tania writes extremely clear, concise tutorials that have the best ratio of learning to bullshit that I've encountered
