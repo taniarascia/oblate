b1069d42c2fe2221d9600ae35e07d7b87101f9ff
Tania Rascia <taniarascia@gmail.com>
2018-07-19 15:08:51 -0500

    Fix link error

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -26,14 +26,16 @@
 			<?php the_content(); ?>
 
 			<div class="author">
-				<h2>Honesty, quality, morality on the web</h2>
+				<h2>Quality and morality on the web</h2>
 				<p>Hi, my name is <strong>Tania Rascia</strong>. I have turned down every advertiser, sponsored post, and affiliate who has come to me. <strong>I give away all my knowledge for free to my <mark>300,000+ monthly readers</mark></strong> and helped thousands of people learn and successfully transition into a design/development career.</p>
 
 				<p><strong>You know what I stand for, and you know my commitment to you - no bullshit, no sponsored posts, no pop-ups, no blocked content, schemes, tactics, or gimmicks. Just free, quality content. <mark>Support me by buying me a coffee</marK></strong></p>
 				
 				<div class="author-options">
+					<div>
 					<h3>I really appreciate your support!</h3>
 					<p><a class="button paypal-button" href="http://ko-fi.com/taniarascia" target="_blank">Buy me a coffee</a></p>
+					</div>
 				</div>
 			</div>
 
