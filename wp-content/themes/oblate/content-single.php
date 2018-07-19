c39c5cf29ce9c2637d71573cdf50ce0becfd9c8e
Tania Rascia <taniarascia@gmail.com>
2018-07-19 15:06:46 -0500

    Coffee

--- wp-content/themes/oblate/content-single.php
+++ wp-content/themes/oblate/content-single.php
@@ -26,30 +26,14 @@
 			<?php the_content(); ?>
 
 			<div class="author">
-				<h2>Honesty, quality, morality</h2>
-				<p>Hi, my name is <strong>Tania Rascia</strong>. Every single article on this website was written by me, for you. There's something I would like you to know.</p>
+				<h2>Honesty, quality, morality on the web</h2>
+				<p>Hi, my name is <strong>Tania Rascia</strong>. I have turned down every advertiser, sponsored post, and affiliate who has come to me. <strong>I give away all my knowledge for free to my <mark>300,000+ monthly readers</mark></strong> and helped thousands of people learn and successfully transition into a design/development career.</p>
 
-				<ul>
-				<li>I have turned down every individual or company offering to write a <strong>sponsored post</strong> on my website. </li>
-				<li>I have turned down every advertiser offering money in exchange for <strong>displaying their ads</strong> on my website.</li>
-				<li>I have turned down the option of displaying <strong>paid affiliate links</strong> on my website.</li>
-				</ul>
-
-				<p>I don't use pop-ups, sell any products, or block my content. I give away all my knowledge for free to my 300,000+ monthly readers.</p>
-
-				<p>I want to make my website a bastion of knowledge and hope. I want to keep sharing my knowledge and helping thousands of people learn and successfully transition into a design/development career.</p>
-
-				<p><strong>If you support what I do, or if an article I have written has helped guide you in your learning journey, <mark>help me keep this website ad-free, updated, and wonderful</mark>. Support me by being a Patron or buying me a coffee.</strong></p>
+				<p><strong>You know what I stand for, and you know my commitment to you - no bullshit, no sponsored posts, no pop-ups, no blocked content, schemes, tactics, or gimmicks. Just free, quality content. <mark>Support me by buying me a coffee</marK></strong></p>
 				
 				<div class="author-options">
-					<div>
-						<h3>Be a Patron</h3>
-						<p><a class="button paypal-button" href="https://www.patreon.com/taniarascia" target="_blank">Be a patron</a></p>
-					</div>
-					<div>
-						<h3>Donate</h3>
-						<p><a class="button donate-button" href="https://paypal.me/taniarascia/5">$5</a> <a class="button" href="https://paypal.me/taniarascia/10">$10</a> <a class="button donate-button" href="https://paypal.me/taniarascia/20">$20</a></p>
-					</div>
+					<h3>I really appreciate your support!</h3>
+					<p><a class="button paypal-button" href="http://ko-fi.com/taniarascia" target="_blank">Buy me a coffee</a></p>
 				</div>
 			</div>
 
