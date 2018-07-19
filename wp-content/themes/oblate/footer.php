577c90979e92618c87aed296e119d482b47b417d
Tania Rascia <taniarascia@gmail.com>
2018-07-19 16:42:44 -0500

    Slack

--- wp-content/themes/oblate/footer.php
+++ wp-content/themes/oblate/footer.php
@@ -7,11 +7,11 @@
      <p>
        <a href="<?php echo site_url(); ?>/me">About me</a>&nbsp;|&nbsp; 
        <a href="<?php echo site_url(); ?>/blog">Archive</a>&nbsp;|&nbsp;
-       <a href="https://twitter.com/taniarascia">Twitter</a>&nbsp;|&nbsp;
+       <a href="https://join.slack.com/t/httpchat/shared_invite/enQtNDAxODEwMTU2ODM0LTljMGRjZDZmZTA1ZDEwNjc5M2QwYjk5ZjViMGUzNDYzZjJhMGM2OTNmNTNkODM1OWYzZWIzYjA2NTU4YTczZWU">HTTPChat Slack</a>&nbsp;|&nbsp;
        <a href="<?php echo site_url(); ?>/contact">Contact</a>
       </p>
-    <p>I'm <a href="<?php echo site_url(); ?>/me">Tania</a>, a <strong>designer, developer, writer, and former chef</strong> from Chicago. I currently work full-time as a web developer, and sometimes I write for <a href="https://www.digitalocean.com/community/tutorial_series/how-to-code-in-javascript">DigitalOcean</a> and SitePoint. I love to create things for the web.</p>
-    <p>Like what you see on this site? Help me keep it ad-free and wonderful. <a href="https://www.patreon.com/taniarascia" target="_blank">Be a patron</a> or <a href="https://paypal.me/taniarascia/5">buy me a coffee.</a></p>
+    <p>I'm <a href="<?php echo site_url(); ?>/me">Tania</a>, a <strong>designer, developer, writer, and former chef</strong> from Chicago. I love to create things for the web.</p>
+    <p>Like what you see on this site? Help me keep it ad-free and wonderful - <a href="https://paypal.me/taniarascia/5">buy me a coffee.</a></p>
 		
 	 </div>
 	 <div class="footer-support">
